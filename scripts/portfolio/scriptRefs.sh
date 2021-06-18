14fa58bc994b75a9989d39d653271c12115cd4aa
Mamun Khan <mamun_k@outlook.com>
2021-06-19 02:08:22 +1000

    Added script called scriptRefs.sh that goes through the scripts in each of the directories and outputs the header comments to allow me to know what kind of things are done in each script.  Adding an argument when calling scriptRefs.sh will cause it to only display header comments that include the argument text.

---
+++ scripts/portfolio/scriptRefs.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+#scriptList=$(tree | grep .sh)
+treeJson=$(tree -iJ | jq -r '.[]')
+isType=0
+isName=0
+isDirectory=0
+isFile=0
+
+for ln in $treeJson
+do
+	if [ 1 -eq $isName ]; then # if this line is a name
+		if [ 1 -eq $isDirectory ]; then
+			ln="${ln%,}"	# remove trailing comma
+			ln="${ln%\"}"	# remove trailing quote
+			ln="${ln#\"}"	# remove leading quote
+			cd $ln
+		elif [ 1 -eq $isFile ]; then
+			ln="${ln%\"}"	# remove trailing quote
+			ln="${ln#\"}"	# remove leading quote
+			if [[ $ln == *".sh" ]]; then
+				if (( $#==1 )); then # an arg has been passed.  only find files relating to this arg
+					cat $ln | grep '###' | grep -v 'cat $ln | grep' | grep -q $1
+					if [ $? -eq 0 ]; then
+						cat $ln | grep '###' | grep -v 'cat $ln | grep'
+						echo
+					fi
+				else
+					# output the description comment lines of the 
+					# script and ignore this line itself 
+					cat $ln | grep '###' | grep -v 'cat $ln | grep'
+					if [ $? -eq 0 ]; then
+						echo # break up the output with spaces
+							# but only if the previous line
+							# output something
+					fi
+				fi
+			# else file is not a script
+			fi
+		fi
+		#reset
+		isDirectory=0
+		isFile=0
+		isName=0
+	elif [ "\"name\":" = $ln ]; then # next line is the name
+		isName=1
+	elif [ 1 -eq $isType ]; then # if this line is a type
+		if [ "\"directory\"," = $ln ]; then
+			isDirectory=1
+		elif [ "\"file\"," = $ln ]; then
+			isFile=1
+		# else we're not interested
+		fi
+		isType=0
+	elif [ "\"type\":" = $ln ]; then # next line is a type
+		isType=1
+	elif [[ $ln == *"]" ]]; then # end of a directory
+		cd ..
+	# else ignore the line
+	fi
+done
+
+exit 0
