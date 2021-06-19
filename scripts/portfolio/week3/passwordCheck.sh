8afe1bceedb74b9d49da2f00d0e48f596a4b6a3f
Mamun Khan <mamun_k@outlook.com>
2021-06-19 16:43:26 +1000

    updated passwordCheck.sh to use colours

--- scripts/portfolio/week3/passwordCheck.sh
+++ scripts/portfolio/week3/passwordCheck.sh
@@ -6,9 +6,14 @@
 ### - use '-eq' test within 'if' conditional
 ### - return an exit code
 
+redTxt='\033[0;31m'
+grnTxt='\033[0;32m'
+ltRdTxt='\033[1;31m'
+noCol='\033[0m'
+
 # prompt user to enter in password
 # -s flag for silent entry
-read -sp "Please enter in your password: " pwd
+read -sp "$(echo -e ${ltRdTxt}Enter Password: ${noCol})" pwd
 
 #compare hash of password offered to that of the one kept in the secret.txt file
 echo $pwd | sha256sum -c --status secret.txt
@@ -24,11 +29,11 @@ exitCode=1
 
 #if the password hashes match, print "Access Granted" and change the exit code to success
 if [ 0 -eq $result ]; then
-	echo "Access Granted"
+	echo -e "${grnTxt}Access Granted${noCol}"
 	exitCode=0
 else
 	#  authentication failed
-	echo "Access Denied"
+	echo -e "${redTxt}Access Denied${noCol}"
 fi
 
 exit $exitCode
