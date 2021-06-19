16b5a0c727b284f7b7e8fbfc877f34aa52fc4e0b
Mamun Khan <mamun_k@outlook.com>
2021-06-19 17:22:04 +1000

    Updated MegaMenu.sh to include a basic history feature as a user friendliness improvement.  No suggestions for other improvements were made in unit blog unfortunately

--- scripts/portfolio/week3/MegaMenu.sh
+++ scripts/portfolio/week3/MegaMenu.sh
@@ -4,13 +4,21 @@
 ### - call another script within a script and pass arguments
 ### - use 'case' conditional
 ### - use '*)' for default case in 'case' conditional
+### - a basic command history implementation
 
 #run passwordCheck.sh
 ./passwordCheck.sh
 
+purpTxt='\033[1;35m'
 blueTxt='\033[0;34m'
 cyanTxt='\033[0;36m'
 noCol='\033[0m'
+
+# adding the ability to view history because I thought it'd be cool
+# won't be super complex due to time constraints
+# ideally would be good to be able to also capture inputs to the 
+#	scripts being called, but I'll leave that for later
+historyFile=".megaMenuHistory"
 
 # check that password check succeeded
 if [ $? -eq 0 ] ; then
@@ -25,37 +33,54 @@ if [ $? -eq 0 ] ; then
 		echo "5. Create Week Folders"
 		echo "6. Check Filenames"
 		echo -e "7. Download a File${noCol}"
-		echo "8. Exit"
+		echo -e "${purpTxt}8. View command history${noCol}"
+		echo "9. Exit"
 		read -p ">" cmd
 		case "$cmd" in
-			1)
-				# create a folder
+			1)	# create a folder
+				echo "1. Create a folder" >> $historyFile
 				./foldermaker.sh
 				;;
 			2)	# copy a folder
+				echo "2. Copy a folder" >> $historyFile
 				./foldercopier.sh
 				;;
 			3)	# set a new password
+				echo "3. Set a password" >> $historyFile
 				./setPassword.sh
 				;;
 			4)	# calculator
+				echo "4. Calculator" >> $historyFile
 				./bashCalc.sh
 				;;
 			5)	# create week folders
 				read -p "enter the starting and ending week numbers separated by a space: " weeks
+				weekArgs=($weeks)
+				#TODO - sanitize input
+				#	- check that both inputs are numbers
+				#	- check that first number is lower than second number
+				echo "5. Create Week folders for weeks ${weekArgs[0]} to ${weekArgs[1]}" >> $historyFile
 				./megafoldermaker.sh $weeks
 				;;
 			6)	# check filenames
 				read -p "enter the file containing the names: " filename
+				echo "6. Check filenames in $filename" >> $historyFile
 				./filenames.sh $filename
 				;;
 			7)	# download a file
+				echo "7. Download a file" >> $historyFile
 				./InternetDownloader.sh
 				;;
-			8)	# exit
+			8)	# history
+				cat $historyFile
+				echo "8. View command history" >> $historyFile
+				;;
+			9)	# exit
+				echo "9. Exit" >> $historyFile
 				# do nothing - the 'until' loop will take care of it
 				;;
 			*)	echo "you have not chosen from the list"
+				echo "Unknown option: $cmd" >> $historyFile
 				;;
 		esac
 	done
